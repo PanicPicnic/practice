d212d92e0f58be2bb822e6799df035bd0cd3b905
Nikolay.kalenchenko <nikolay.kalenchenko@playtika.com>
2020-08-08 11:26:16 +0300

    Initial commit

---
+++ src/Practice.java
@@ -0,0 +1,18 @@
+
+
+public class Practice {
+    public static void main(String[] args) {
+        int value = 1023;
+    while ((value%2==0)&&value>2){
+        value = value/2;}
+        if (value/2 == 1){
+            System.out.println("is a power of 2");
+        }
+        else{
+            System.out.println("not a power of 2");
+        }
+
+
+
+    }
+    }
