b2ec5be5c6ada2f4867ff678781af8a39a4f1919
Nikolay.kalenchenko <nikolay.kalenchenko@playtika.com>
2020-08-11 20:02:56 +0300

    pract

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+
+public class Calculator {
+
+    public static int calculate ( int a, int b, int c){
+    return (a+b+c)/3;
+    }
+
+    public static void main(String[] args) {
+        int a = 2, b=4, c=6;
+        System.out.println(calculate(a,b,c));
+    }
+}
